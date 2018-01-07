d81c6d9d707c82e2d98e7ec898afde80070ac588
Ben <i@auburn.edu>
2018-01-07 16:44:10 -0600

    Init

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+
+  name: string;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.name = 'john doe';
+
+  }
+
+}
