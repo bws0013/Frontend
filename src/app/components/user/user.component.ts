9d5d430240a1c57650477673913db01ecdebc343
Ben <i@auburn.edu>
2018-01-07 20:23:10 -0600

    Adding more basics

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -8,12 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class UserComponent implements OnInit {
 
   name: string;
+  age: number;
+  location: string;
+
+  hobbies: string[];
 
   constructor() { }
 
   ngOnInit() {
     this.name = 'john doe';
+    this.age = 50;
+    this.location = 'tuvalu';
 
+    this.hobbies = ['fishing', 'golfing', 'coding'];
   }
 
 }
