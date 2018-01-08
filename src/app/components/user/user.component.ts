def9e38513d0e7b56215cdced7a3c2aec9968f61
Ben <i@auburn.edu>
2018-01-08 17:45:52 -0600

    We now have forms

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -23,4 +23,22 @@ export class UserComponent implements OnInit {
     this.hobbies = ['fishing', 'golfing', 'coding'];
   }
 
+  onClick() {
+    this.name = 'is this the real life';
+    this.hobbies.push('new hobby');
+  }
+
+  addHobby(hobby) {
+    this.hobbies.unshift(hobby);
+    return false;
+  }
+
+  deleteHobby(hobby) {
+    for (let i = 0; i < this.hobbies.length; i++) {
+      if (this.hobbies[i] === hobby) {
+        this.hobbies.splice(i, 1);
+      }
+    }
+  }
+
 }
